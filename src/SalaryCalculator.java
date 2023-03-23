5087fe2b48113a1581ac36558474db7c9c6069d2
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-23 21:09:07 +0530

    Add. Added the logic of the jeevitha salary calculator

--- src/SalaryCalculator.java
+++ src/SalaryCalculator.java
@@ -1,5 +1,66 @@
+import java.util.Scanner;
+
 public class SalaryCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] inputForWeek = new int[7];
+
+        System.out.println("Enter the Working Hours Of Jeevitha: ");
+        for(int iterator = 0;iterator<7;iterator++){
+            inputForWeek[iterator] = scan.nextInt();
+        }
+
+        int totalSalary = 0;
+
+        int totalHours = 0;
+
+        for(int iterator = 0;iterator<inputForWeek.length;iterator++){
+
+            if(iterator == 0){
+
+                totalSalary+=(inputForWeek[iterator]*100)+((inputForWeek[iterator]*100)*0.50);
+
+            }else if(iterator == 6){
+
+                totalSalary+=(inputForWeek[iterator]*100)+((inputForWeek[iterator]*100)*0.25);
+
+            }else{
+
+                if(inputForWeek[iterator]>8){
+
+                    if(totalHours>40){
+
+                        totalSalary+=25;
+
+                    }
+
+                    int extraHours = inputForWeek[iterator]-8;
+
+                    totalSalary+=(inputForWeek[iterator]*100);
+
+                    totalSalary+=(extraHours*15);
+
+                }else{
+
+                    if(totalHours>40){
+
+                        totalSalary+=25;
+
+                    }
+
+                    totalSalary+=(inputForWeek[iterator]*100);
+
+                }
+
+            }
+
+            totalHours+=inputForWeek[iterator];
+
+        }
+
+        System.out.println(totalSalary);
+
     }
 }
